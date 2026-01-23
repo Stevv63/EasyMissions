39e3c1fbae82fa09ea3399d24fcb6dfbe611cea7
Stev6 <157082448+Stevv63@users.noreply.github.com>
2026-01-23 05:19:18 +0200

    Improve ranges by allowing ranges that are "anything greater than this number" and "anything smaller than this number" and make things slightly safer

--- src/main/java/io/github/stev6/easymissions/util/IntRange.java
+++ src/main/java/io/github/stev6/easymissions/util/IntRange.java
@@ -1,6 +1,7 @@
 package io.github.stev6.easymissions.util;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 
 public record IntRange(int min, int max) {
     public static final IntRange ANY_RANGE = IntRange.of(0, Integer.MAX_VALUE);
@@ -12,11 +13,16 @@ public record IntRange(int min, int max) {
     }
 
     public static IntRange fromString(String range) {
-        String[] split = range.split(SPLITTER);
-        int min = Integer.parseInt(split[0]);
-        if (split.length == 1) return of(min,min);
-        int max = Integer.parseInt(split[1]);
-        return of(min,max);
+        if (range.endsWith("+") && range.length() > 1)
+            return of(Integer.parseInt(range.substring(0, range.length() - 1).trim()), Integer.MAX_VALUE);
+        else if (range.startsWith("-") && range.length() > 1)
+            return of(0, Integer.parseInt(range.substring(1).trim()));
+
+        String[] split = range.split(Pattern.quote(SPLITTER), 2);
+        int min = Integer.parseInt(split[0].trim());
+        if (split.length == 1) return of(min, min);
+        int max = Integer.parseInt(split[1].trim());
+        return of(min, max);
     }
 
     public static IntRange of(int min, int max) {
