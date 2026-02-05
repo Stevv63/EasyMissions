9c6a0a05beb56896929c1dde4c0a4674722b3461
Stev6 <157082448+Stevv63@users.noreply.github.com>
2026-02-05 03:15:28 +0200

    Add `equipment` section to match entity equipment (Armor, items held) with ItemDataMatchers

--- src/main/java/io/github/stev6/easymissions/matcher/impl/EntityDataMatcher.java
+++ src/main/java/io/github/stev6/easymissions/matcher/impl/EntityDataMatcher.java
@@ -4,10 +4,8 @@ import io.github.stev6.easymissions.exception.ConfigException;
 import io.github.stev6.easymissions.matcher.ValueMatcher;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Ageable;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Monster;
+import org.bukkit.entity.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -20,7 +18,7 @@ public class EntityDataMatcher implements ValueMatcher<Entity> {
         this.predicate = predicate;
     }
 
-    public static EntityDataMatcher parse(ConfigurationSection section) {
+    public static EntityDataMatcher parse(@NotNull ConfigurationSection section) {
         Predicate<Entity> predicate = Objects::nonNull;
 
         HashSet<String> types = new HashSet<>(section.getStringList("types"));
@@ -37,6 +35,36 @@ public class EntityDataMatcher implements ValueMatcher<Entity> {
         if (adultOnly) predicate = predicate.and(e -> e instanceof Ageable ageable && ageable.isAdult());
         if (babyOnly) predicate = predicate.and(e -> e instanceof Ageable ageable && !ageable.isAdult());
 
+        ConfigurationSection equipment = section.getConfigurationSection("equipment");
+        if (equipment != null) {
+            ConfigurationSection mainhand = equipment.getConfigurationSection("mainhand");
+            ConfigurationSection offhand = equipment.getConfigurationSection("offhand");
+            ConfigurationSection helmet = equipment.getConfigurationSection("helmet");
+            ConfigurationSection chestplate = equipment.getConfigurationSection("chestplate");
+            ConfigurationSection leggings = equipment.getConfigurationSection("leggings");
+            ConfigurationSection boots = equipment.getConfigurationSection("boots");
+
+            ItemDataMatcher mainhandMatcher = mainhand != null ? ItemDataMatcher.parse(mainhand) : null;
+            ItemDataMatcher offhandMatcher = offhand != null ? ItemDataMatcher.parse(offhand) : null;
+            ItemDataMatcher helmetMatcher = helmet != null ? ItemDataMatcher.parse(helmet) : null;
+            ItemDataMatcher chestMatcher = chestplate != null ? ItemDataMatcher.parse(chestplate) : null;
+            ItemDataMatcher leggingsMatcher = leggings != null ? ItemDataMatcher.parse(leggings) : null;
+            ItemDataMatcher bootsMatcher = boots != null ? ItemDataMatcher.parse(boots) : null;
+
+            predicate = predicate.and(e -> {
+                if (!(e instanceof LivingEntity lE)) return false;
+
+                var eq = lE.getEquipment();
+                if (eq == null) return false;
+
+                if (mainhandMatcher != null && !mainhandMatcher.matches(eq.getItemInMainHand())) return false;
+                if (offhandMatcher != null && !offhandMatcher.matches(eq.getItemInOffHand())) return false;
+                if (helmetMatcher != null && !helmetMatcher.matches(eq.getHelmet())) return false;
+                if (chestMatcher != null && !chestMatcher.matches(eq.getChestplate())) return false;
+                if (leggingsMatcher != null && !leggingsMatcher.matches(eq.getLeggings())) return false;
+                return bootsMatcher == null || bootsMatcher.matches(eq.getBoots());
+            });
+        }
 
         boolean monsterOnly = section.getBoolean("monster", false);
         if (monsterOnly) predicate = predicate.and(e -> e instanceof Monster);
